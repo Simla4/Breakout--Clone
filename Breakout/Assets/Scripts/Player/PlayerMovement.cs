c92004a78b142a9be1b461d48eff6fd4a4176009
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-02-03 16:53:10 +0300

    feat: player following mouse

---
+++ Breakout/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float sideLimits = 11;
+
+    #endregion
+
+    #region Callbacks
+
+    private void Update()
+    {
+        var currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);;
+        
+        MovePlayer(currentMousePos.x);
+    }
+
+    #endregion
+    
+    #region OtherMethods
+
+    private void MovePlayer(float mousePosX)
+    {
+        var pos = transform.position;
+
+        pos.x = mousePosX;
+        var newPosX = Mathf.Clamp(pos.x, -sideLimits, sideLimits);
+        
+        transform.position = new Vector3(newPosX, pos.y, pos.z);
+    }
+
+    #endregion
+}
