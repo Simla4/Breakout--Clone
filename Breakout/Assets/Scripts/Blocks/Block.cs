ed42c8df1498772113a0d731d5244982e2d07f04
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-02-15 00:56:55 +0300

    feat: destroy the block when ball collision

--- Breakout/Assets/Scripts/Blocks/Block.cs
+++ Breakout/Assets/Scripts/Blocks/Block.cs
@@ -1,23 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Block : MonoBehaviour
+public class Block : MonoBehaviour, ISpawn, IDespawn
 {
     #region Variables
 
     [SerializeField] private BlockData blockData;
-
     [SerializeField] private SpriteRenderer blockSprite;
 
+    private Pool<Block> blockPool;
+
     #endregion
-    // Start is called before the first frame update
-    void Start()
+
+    #region
+
+    private void Start()
     {
+        blockPool = PoolManager.Instance.blockPool;
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnBallCollisionBlock += RemoveBlock;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnBallCollisionBlock -= RemoveBlock;
+    }
+
+    #endregion
+
+    #region OtherMethods
+
+    private void RemoveBlock()
+    {
+        blockPool.ReturnToPool(this);
+    }
+
+    public void OnSpawn()
+    {
+        gameObject.SetActive(true);
+        
         var blockSpriteCount = blockData.blockSpriteList.Count;
         
         var rnd = Random.Range(0, blockSpriteCount);
 
         blockSprite.sprite = blockData.blockSpriteList[rnd];
     }
+    
+    public void OnDespawn()
+    {
+        gameObject.SetActive(false);
+    }
+
+    #endregion
 }
