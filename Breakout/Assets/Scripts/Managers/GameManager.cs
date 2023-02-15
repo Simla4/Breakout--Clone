3de00d067b3760121da2fdd770a64d9c86fb1734
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-02-15 15:01:07 +0300

    feat: calculate score

--- Breakout/Assets/Scripts/Managers/GameManager.cs
+++ Breakout/Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+
+    [SerializeField] private int health = 3;
+
+    #endregion
+
+    #region Callbacks
+
+    private void Start()
+    {
+        PlayerPrefs.GetInt("Score", 0);
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnBallCollisionBlock += CalculateScore;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnBallCollisionBlock -= CalculateScore;
+    }
+
+    #endregion
+
+    #region OtherMethods
+
+    private void CalculateScore()
+    {
+        var score = PlayerPrefs.GetInt("Score", 0);
+
+        score += 10;
+
+        PlayerPrefs.SetInt("Score", 0);
+        
+        Debug.Log("Score: " + score);
+    }
+
+    private void Health()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #endregion
 }
