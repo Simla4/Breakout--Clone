720b5441da290fe7baaae84fc85bb5d57b88f835
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-02-15 19:03:33 +0300

    feat: Chage score in UI

--- Breakout/Assets/Scripts/Managers/UIManager.cs
+++ Breakout/Assets/Scripts/Managers/UIManager.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+
+    [SerializeField] private GameObject startUI;
+    [SerializeField] private GameObject inGameUI;
+    [SerializeField] private GameObject failUI;
+    [SerializeField] private GameObject winUI;
+
+    [SerializeField] private TextMeshProUGUI scoreTxt;
+
+    #endregion
+
+    #region Callbacks
+
+    private void OnEnable()
     {
-        
+        EventManager.OnBallCollisionBlock += ChangeScoreTxt;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        EventManager.OnBallCollisionBlock -= ChangeScoreTxt;
     }
+
+    #endregion
+
+    #region OtherMethods
+
+    private void ChangeScoreTxt()
+    {
+        scoreTxt.text = PlayerPrefs.GetInt("Score", 0).ToString();
+    }
+
+    #endregion
 }
