409bd9d8c9d4b050dca68e8f157f07228ec65442
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-02-22 13:48:58 +0300

    feat: level generating

--- Breakout/Assets/Scripts/Managers/LevelManager.cs
+++ Breakout/Assets/Scripts/Managers/LevelManager.cs
@@ -7,6 +7,9 @@ public class LevelManager : MonoBehaviour
 {
     #region Variables
 
+    [SerializeField] private int blockCount = 10;
+    [SerializeField] private Vector2 distance = new Vector2(2.8f, -0.8f);
+    
     private Pool<Block> blockPool;
 
     #endregion
@@ -17,15 +20,36 @@ public class LevelManager : MonoBehaviour
     {
         blockPool = PoolManager.Instance.blockPool;
 
-        blockPool.Spawn();
-        blockPool.Spawn();
+        CreateLevel();
     }
 
     #endregion
 
     #region OtherMethods
 
-    
+    private void CreateLevel()
+    {
+        var firstPos = new Vector2(-7, 3.8f);
+        
+        for (int i = 0; i < blockCount; i++)
+        {
+            
+            var block = blockPool.Spawn();
+            block.transform.position = firstPos;
+
+            firstPos.x += distance.x;
+
+            //firstPos = new Vector2(firstPos.x, firstPos.y);
+            
+            Debug.Log("first Pos: " + firstPos);
+
+            if (block.transform.position.x >= 7)
+            {
+                firstPos = new Vector2(-7, firstPos.y - distance.y);
+            }
+            
+        }
+    }
 
     #endregion
 }
